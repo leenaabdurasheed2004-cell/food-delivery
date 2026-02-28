324c1757008d7ad7b350ffe46348f0d94e56f41f
leenaabdurasheed2004-cell <leenaabdurasheed2004@gmail.com>
2026-02-28 12:42:12 +0530

    Update backend URL in StoreContext

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext=createContext(null)
 
 const StoreContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState({})
-    const url = "https://food-del-backend1-55se.onrender.com"
+    const url = "https://food-del-backend-58rk.onrender.com"
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([])
 
